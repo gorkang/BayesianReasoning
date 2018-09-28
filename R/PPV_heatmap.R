e010c8587480477ab5358e953612f768b148cc7a
Gorka <gorkang@gmail.com>
2018-09-28 11:33:16 -0300

    big rewriting. Modularize, include area overlay, etc.

--- R/PPV_heatmap.R
+++ R/PPV_heatmap.R
@@ -8,27 +8,30 @@
 #' @param overlay Show overlay: TRUE / FALSE
 #' @param overlay_labels Lables for each point in the overlay. For example: c("80", "70", "60", "50", "40", "30", "20  y.o.")
 #' @param overlay_position_FP FP value (position in the x-axis) for each point in the overlay. For example: c(7, 8, 9, 12, 14, 14)
+#' @param uncertainty_prevalence How much certainty we have about the prevalence ["high"/"low"]
 #' @param overlay_position_Prevalence Prevalence value (position in the y-axis) for each point in the overlay. For example: c(26, 29, 44, 69, 227, 1667)
 #' @param label_title Title for the plot
 #' @param label_subtitle Subtitle for the plot
 #' @param Language Language for the plot labels: "sp" / "en"
 #' @param save_plot Should save the plot as .png or just show it?  [TRUE/FALSE]
+#' @param PPV_NPV Should show PPV or NPV [PPV/NPV]
 #'
 #' @return A .png plot in the /output folder, or shows a plot
 #' @export
-#' @importFrom ggplot2 ggplot aes geom_tile scale_x_continuous scale_y_continuous scale_fill_gradientn labs margin annotate
+# @importFrom BayesianReasoning .createPPVmatrix
+#' @importFrom ggplot2 ggplot aes geom_tile scale_x_continuous scale_y_continuous scale_fill_gradientn labs margin annotate ggsave
 #' @importFrom reshape2 melt
 #'
 #' @examples
 #' 
 #' # Simple plot
-#' PPV_heatmap(Max_Prevalence = 500, Sensitivity = 90, Max_FP = 15, Language = "en", overlay = FALSE)
+#' PPV_heatmap(Max_Prevalence = 500, Sensitivity = 90, Max_FP = 15)
 #' 
 #' # Show a plot with an overlay
 #' PPV_heatmap(Max_Prevalence = 1800, Sensitivity = 90, Max_FP = 15,
 #'             label_subtitle = "PPV of Mammogram for Breast Cancer by Age",
-#'             save_plot = TRUE, Language = "en",
-#'             overlay = TRUE,
+#'             save_plot = FALSE, Language = "en",
+#'             overlay = "line",
 #'             overlay_labels = c("80", "70", "60", "50", "40", "30", "20  y.o."),
 #'             overlay_position_FP = c(6.5, 7, 8, 9, 12, 14, 14),
 #'             overlay_position_Prevalence = c(22, 26, 29, 44, 69, 227, 1667))
@@ -36,20 +39,41 @@
 #' # Another plot with an overlay
 #' PPV_heatmap(Max_Prevalence = 1200, Sensitivity = 81, Max_FP = 5,
 #'             label_subtitle = "Prenatal screening for Down Syndrome by Age",
-#'             save_plot = TRUE, Language = "en",
-#'             overlay = TRUE,
+#'             save_plot = FALSE, Language = "en",
+#'             overlay = "line",
 #'             overlay_labels = c("40 y.o.", "35 y.o.", "30 y.o.", "25 y.o.", "20 y.o."),
 #'             overlay_position_FP = c(4.8, 4.8, 4.8, 4.8, 4.8),
 #'             overlay_position_Prevalence = c(68, 249, 626, 946, 1068))
+
+#' # A plot with a point overlay
+#' PPV_heatmap(Max_Prevalence = 1200, Sensitivity = 81, Max_FP = 5,
+#'             label_subtitle = "Prenatal screening for Down Syndrome by Age",
+#'             overlay = "area",
+#'             overlay_labels = "40 y.o.",
+#'             overlay_position_FP = 4.8,
+#'             overlay_position_Prevalence = 68
 PPV_heatmap <- function(Max_Prevalence, Sensitivity, Max_FP,
-                            overlay = FALSE, overlay_labels, overlay_position_FP, overlay_position_Prevalence, 
+                            overlay = "no", overlay_labels, overlay_position_FP, overlay_position_Prevalence, uncertainty_prevalence = "high",
                             label_title = "", label_subtitle = "",
                             Language = "en", save_plot = FALSE,
                             PPV_NPV = "PPV") {
   
   # Libraries ---------------------------------------------------------------
-  source("R/.createPPVmatrix.R", local = TRUE)
 
+  # message(getwd())
+  # cat(message(getwd()))
+  # print(message(getwd()))
+  # stop()
+  
+  # Absolute paths
+  source("/home/emrys/gorkang@gmail.com/RESEARCH/PROYECTOS-Code/R_PPV_Plot/R/.createPPVmatrix.R", local = TRUE)
+  source("/home/emrys/gorkang@gmail.com/RESEARCH/PROYECTOS-Code/R_PPV_Plot/R/.check_size.R", local = TRUE)
+  source("/home/emrys/gorkang@gmail.com/RESEARCH/PROYECTOS-Code/R_PPV_Plot/R/.plot_creation.R", local = TRUE)
+  
+  # Relative paths
+  # source("R/.createPPVmatrix.R", local = TRUE)
+  # source("R/.check_size.R", local = TRUE)
+  # source("R/.plot_creation.R", local = TRUE)
   
   # DEBUG -------------------------------------------------
     
@@ -69,7 +93,7 @@ PPV_heatmap <- function(Max_Prevalence, Sensitivity, Max_FP,
       
       #GRAPHIC Parameters *************
 
-      modifier_text_overlay_position = (Max_Prevalence/75)
+      # modifier_text_overlay_position = (Max_Prevalence/75)
   
       #Labels 
       if (Language == "sp") {
@@ -90,29 +114,12 @@ PPV_heatmap <- function(Max_Prevalence, Sensitivity, Max_FP,
         
       }
   
-      if (overlay == TRUE) {
+      if (overlay != "no") {
         filename_overlay = "_overlay"
       } else {
         filename_overlay = ""
       }
       
-      
-      # CHECKS ------------------------------------------------------------------
-      
-      # If one of the overlay values is bigger than the test/disease values, we use the bigger value
-      if (overlay == TRUE) {
-        
-        if (Max_FP < max(overlay_position_FP)) {
-          Max_FP = max(overlay_position_FP)
-          warning("\n\n  * One of the overlay_position_FP values is bigger than Max_FP. We use the max(overlay_position_FP) value to plot.")
-        }
-        
-        if (Max_Prevalence <= max(overlay_position_Prevalence)) {
-          Max_Prevalence = (max(overlay_position_Prevalence) +  (modifier_text_overlay_position))
-          warning("\n\n  * One of the overlay_position_Prevalence is bigger than Max_Prevalence. We use the max(overlay_position_Prevalence) value to plot.")
-        }
-      }
-  
       
 
   # Create PPV matrix -------------------------------------------------------
@@ -121,50 +128,111 @@ PPV_heatmap <- function(Max_Prevalence, Sensitivity, Max_FP,
   
   # PLOT --------------------------------------------------------------------
   
-      #HEATMAP
-      Paleta_DV = c( "white", "grey", "gray30", "yellowgreen", "chartreuse4")
-
-      breaks_DV = c(0, 0.25, 0.5, 0.75, 1)
-      labels_DV = c(0, 25, 50, 75, 100)
+    # Heatmap
+  
+      .plot_creation(PPV_melted)
+ 
+  
+    # Overlay
+  
+      if (overlay == "line") {
         
-      breaks_x = seq(0, Max_FP, Step_size_FP * 10)
-      labels_x = paste0(seq(Min_FP, Max_FP, Step_size_FP * 10), "%")
-      breaks_y = seq(0, Max_Prevalence, Step_size_Prevalence * 10)
-      labels_y = paste(prevalence_label, round(seq(Min_Prevalence - 1, Max_Prevalence, Step_size_Prevalence * 10),0))[-1]
-      labels_y = c(paste(prevalence_label, "1"), labels_y) #We want the legend to start on 1 out of 1
-      
-      
-      # PLOT
-      p <<- ggplot(PPV_melted, aes(melted_FP, melted_Prevalence)) + 
-        geom_tile(aes(fill = melted_PPV), colour = "white") +
-        scale_x_continuous(breaks = breaks_x, labels = labels_x, expand = c(0,0)) + 
-        scale_y_continuous(breaks = breaks_y, labels = labels_y, expand = c(0,0)) +
-        scale_fill_gradientn(colours = Paleta_DV, na.value = "transparent", breaks = breaks_DV, labels = labels_DV, limits = c(0,1), name = legend_label) +
-        theme(text = element_text(size = 20), axis.title.y = element_text(margin = margin(0,10,0,0)), axis.title.x = element_text(margin = margin(10,0,0,0))) +
-        labs(title = label_title,
-             subtitle = label_subtitle, 
-             caption = label_caption,
-             x = x_axis_label, 
-             y = y_axis_label) 
-        
-      
-      if (overlay == TRUE) {
-        
-          # overlay_labels = c("80", "70", "60", "50", "40", "30 y.o.")
-          # overlay_position_FP = c(7, 8, 9, 12, 14)
-          # overlay_position_Prevalence = c(26, 29, 44, 69, 227)
-        
-          overlay_position_x_end = c(overlay_position_FP[1], overlay_position_FP[-length(overlay_position_FP)])
-          overlay_position_y_end = c(overlay_position_Prevalence[1], overlay_position_Prevalence[-length(overlay_position_Prevalence)])
+          # We made the modifiers proportional to the parameters (Max_Prevalence, Max_FP)
+          if (exists("size_uncertainty_area") == FALSE) {size_uncertainty_area = 0}
+          if (abs(Max_Prevalence - max(overlay_position_Prevalence)) > 10) {
+            modifier_text_overlay_position = (Max_Prevalence * size_uncertainty_area  + 1)
+          } else {
+            modifier_text_overlay_position = -(Max_Prevalence * size_uncertainty_area + 1)
+          }
+            # overlay_labels = c("80", "70", "60", "50", "40", "30 y.o.")
+            # overlay_position_FP = c(7, 8, 9, 12, 14)
+            # overlay_position_Prevalence = c(26, 29, 44, 69, 227)
           
-          p = p + annotate("segment", x = overlay_position_FP, xend = overlay_position_x_end, 
-                       y = overlay_position_Prevalence, yend = overlay_position_y_end,
-                       color = "red", alpha = .1, size = 3) +
-                  annotate("text", x = overlay_position_FP, y = overlay_position_Prevalence + modifier_text_overlay_position, label = overlay_labels, size = 4) 
-      }
-      
-      
+            overlay_position_x_end = c(overlay_position_FP[1], overlay_position_FP[-length(overlay_position_FP)])
+            overlay_position_y_end = c(overlay_position_Prevalence[1], overlay_position_Prevalence[-length(overlay_position_Prevalence)])
+            
+            # Add overlay
+            p = p + annotate("segment", x = overlay_position_FP, xend = overlay_position_x_end, 
+                         y = overlay_position_Prevalence, yend = overlay_position_y_end,
+                         color = "red", alpha = .1, size = 3) +
+                    annotate("text", x = overlay_position_FP, y = overlay_position_Prevalence, label = overlay_labels, size = 4) 
+            
 
+      } else if (overlay == "area") {
+  
+        
+          # Uncertainty - how big the square should be
+        
+            if (uncertainty_prevalence == "high") {
+              size_uncertainty_area = .10
+              modifier_overlay_position_x = (Max_FP * size_uncertainty_area)
+            } else if (uncertainty_prevalence == "low") {
+              size_uncertainty_area = .05
+              modifier_overlay_position_x = (Max_FP * size_uncertainty_area)
+            } else {
+              size_uncertainty_area = .075
+              modifier_overlay_position_x = (Max_FP * size_uncertainty_area)
+            }
+              
+            modifier_overlay_position_x = round(modifier_overlay_position_x, 1)
+        
+            
+          # Position of text label
+        
+            # We made the modifiers proportional to the parameters (Max_Prevalence, Max_FP)
+            if (exists("size_uncertainty_area") == FALSE) {size_uncertainty_area = 0}
+            
+            if (abs(Max_Prevalence - overlay_position_Prevalence) > 10) {
+              # modifier_text_overlay_position = (Max_Prevalence * size_uncertainty_area  + 1)
+              modifier_overlay_position_y = (Max_Prevalence * size_uncertainty_area  + 1)
+              modifier_text_overlay_position = modifier_overlay_position_y + Max_Prevalence/50
+              
+              
+            } else {
+              # modifier_text_overlay_position = -(Max_Prevalence * size_uncertainty_area + 1)
+              modifier_overlay_position_y = -(Max_Prevalence * size_uncertainty_area + 1)
+              modifier_text_overlay_position = modifier_overlay_position_y + Max_Prevalence/50
+              
+            }
+        
+          
+          # CHECKS ------------------------------------------------------------------
+          # Checks if overlay is outside the PPV matrix range, and changes values
+            .check_size(overlay, Max_Prevalence, overlay_position_Prevalence, modifier_text_overlay_position, Max_FP, overlay_position_FP)
+            
+            # Recalculate PPV Matrix (if overlay outside old matrix, we need to do this)
+            warning("\n\n  *Recalculate PPVMatrix: ", Max_Prevalence, " ", Sensitivity, " ", Max_FP)
+            .createPPVmatrix(Max_Prevalence, Sensitivity, Max_FP)
+            
+              
+          # Create plot again after potential changes in .check_size
+            .plot_creation(PPV_melted)
+  
+          # Check if ymin is > 1
+          if ((overlay_position_Prevalence - modifier_overlay_position_y) < 1) {
+            ymin_overlay = 0
+          } else {
+              ymin_overlay = overlay_position_Prevalence - modifier_overlay_position_y
+            }
+            
+          # Add overlay
+            p = p + 
+              annotate("rect", color = "red", alpha = .1,
+                       xmin = overlay_position_FP - modifier_overlay_position_x, 
+                       xmax = overlay_position_FP + modifier_overlay_position_x, 
+                       ymin = ymin_overlay, 
+                       ymax = overlay_position_Prevalence + modifier_overlay_position_y) +
+              annotate("point", color = "red", alpha = .5, size = 1,
+                       x = overlay_position_FP, 
+                       y = overlay_position_Prevalence) +
+              annotate("text", size = 4,
+                       x = overlay_position_FP, 
+                       y = overlay_position_Prevalence + modifier_text_overlay_position, 
+                       label = overlay_labels) 
+          
+      }
+  
+  
     # Save plot ---------------------------------------------------------------
 
       if (save_plot == TRUE) {
