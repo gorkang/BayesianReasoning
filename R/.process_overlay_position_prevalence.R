1e620e81e50c87f7b3e7d749157b0138792b7681
Gorka <gorkang@gmail.com>
2018-10-24 09:33:21 -0300

    Added sanity checks

--- R/.process_overlay_position_prevalence.R
+++ R/.process_overlay_position_prevalence.R
@@ -3,39 +3,62 @@
   # DEBUG -------------------------------------------------------------------
     # library(dplyr)
     # overlay_position_Prevalence = "86.8%"
-  
+    # overlay_position_Prevalence = "400 out of 200"
   # *************************************************************************
     
   if (exists("overlay_position_Prevalence") == TRUE) {
+  
 
-    if (grepl("out of", overlay_position_Prevalence) == TRUE) {
+    #  x OUT OF y -------------------------------------------------------------
       
-      overlay_prevalence = regmatches(overlay_position_Prevalence, gregexpr("[[:digit:]]+", overlay_position_Prevalence)) %>% 
-        unlist() %>% 
-        as.numeric()
-      
-    } else if (grepl("%", overlay_position_Prevalence)) {
-
-      # If there is a ",", replace by "." (, is the decimal separator in some regions)
-      if (grepl(",", overlay_position_Prevalence)) {
-        overlay_position_Prevalence = gsub(",", ".", overlay_position_Prevalence) 
+      if (grepl("out of", overlay_position_Prevalence) == TRUE) {
+        
+          overlay_prevalence = regmatches(
+            overlay_position_Prevalence,
+            gregexpr("[[:digit:]]+", overlay_position_Prevalence)) %>%
+            unlist() %>%
+            as.numeric()
+          
+    
+          # Checks x OUT OF y -------------------------------------------------------------
+          if (overlay_prevalence[2] < overlay_prevalence[1]) { stop("Impossible overlay_position_Prevalence value: ", overlay_position_Prevalence, ". The first value (", overlay_prevalence[1], ") should be a subset of the second value (", overlay_prevalence[2], ").")}
+          # *******************************************************************************
+          
+    #  x % ----------------------------------------------------------------------
+      } else if (grepl("%", overlay_position_Prevalence)) {
+  
+        # If there is a ",", replace by "." (, is the decimal separator in some regions)
+        if (grepl(",", overlay_position_Prevalence)) {
+          overlay_position_Prevalence = gsub(",", ".", overlay_position_Prevalence) 
+          
+        }
+   
+        # Get rid of "%" and convert to number
+        overlay_prevalence_temp = gsub("%| %", "", overlay_position_Prevalence) %>% as.numeric(.)
+        
+        overlay_prevalence = Min_Prevalence
+        overlay_prevalence[2] = round(Min_Prevalence / (overlay_prevalence_temp / 100), 0)
+        
+        # Checks x%  --------------------------------------------------------------------
+        if (overlay_prevalence_temp > 100 | overlay_prevalence_temp < 0) { stop("Impossible overlay_position_Prevalence value: ", overlay_position_Prevalence, ". It should be a number between 0% and 100%")}
+        # *******************************************************************************
         
       }
- 
-      # Get rid of "%" and convert to number
-      overlay_prevalence_temp = gsub("%| %", "", overlay_position_Prevalence) %>% as.numeric(.)
-      
-      overlay_prevalence = Min_Prevalence
-      overlay_prevalence[2] = round(Min_Prevalence / (overlay_prevalence_temp / 100), 0)
-    }
-  
     
-    if (length(overlay_prevalence) != 2) {
-      stop("***The parameter 'overlay_position_Prevalence' should be x out of y (e.g. '2 out of 100') or x% (e.g. 2%). Now is: ", overlay_position_Prevalence)
-    }
-  
-    overlay_prevalence_1 <<- overlay_prevalence[1]
-    overlay_prevalence_2 <<- overlay_prevalence[2]
+      
+
+    # General check -----------------------------------------------------------
+      if (length(overlay_prevalence) != 2) {
+        stop("***The parameter 'overlay_position_Prevalence' should be x out of y (e.g. '2 out of 100') or x% (e.g. 2%). Now is: ", overlay_position_Prevalence)
+      }
+    # *************************************************************************
+    
+
+    # Output vars -------------------------------------------------------------
+
+      overlay_prevalence_1 <<- overlay_prevalence[1]
+      overlay_prevalence_2 <<- overlay_prevalence[2]
+      
   }
   
 }
