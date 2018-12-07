ebf54ac6f6adbca077163632caf1cc89a59d825f
gorkang <gorkang@gmail.com>
2018-12-07 19:33:44 -0300

    added a few comments.

--- R/.plot_creation.R
+++ R/.plot_creation.R
@@ -22,6 +22,10 @@
     
   # DEBUG -------------------------------------------------------------------
   
+    # label_title = ""
+    # label_subtitle = ""
+    # label_caption = ""
+    
     # # DEBUG -------------------------------------------------------------------
     # if (exists("DEBUG") == FALSE) {DEBUG = 0}
     # 
@@ -40,33 +44,38 @@
     # Global variables -------------------------------------------------------
     
     # Colors, breaks and steps for Prevalence and FP axis
-    Paleta_DV = c( "white", "grey", "gray30", "yellowgreen", "chartreuse4")
+    Paleta_DV = c("white", "grey", "gray30", "yellowgreen", "chartreuse4")
     breaks_DV = c(0, 0.25, 0.5, 0.75, 1)
     labels_DV = c(0, 25, 50, 75, 100)
     
     
     
   # PPV ---------------------------------------------------------------------
-     
+    
     if (PPV_NPV == "PPV") {
-         
+    
+      # Create plot
+      p = ggplot2::ggplot(PPV_melted, ggplot2::aes(FP, (Prevalence)))  
+
         # USE PPV_melted to get this!!!!
         breaks_x = round(seq(from = .GlobalEnv$Min_FP, to = .GlobalEnv$Max_FP, by = .GlobalEnv$Step_size_FP * 10), decimals_x)
         labels_x = paste0(breaks_x, "%")
         
         breaks_y = round(unique(PPV_melted$Prevalence)[c(seq(1, steps_matrix, 10), 101)], decimals_y)
+        # breaks_y = round(unique(PPV_melted$Prevalence)[c(seq(1, steps_matrix, 10), max(PPV_melted$Prevalence))], decimals_y)
         labels_y = paste(Min_Prevalence, .GlobalEnv$prevalence_label, breaks_y)
         # labels_y = paste(Min_Prevalence, prevalence_label, round(unique(PPV_melted$Prevalence)[c(seq(1, 100, 10), 101)], 0))
         
-        # Create plot
-          p = ggplot2::ggplot(PPV_melted, ggplot2::aes(FP, Prevalence)) + 
-            ggplot2::geom_tile(ggplot2::aes(fill = PPV), colour = "white")
+        # PPV tiles
+          p = p + ggplot2::geom_tile(ggplot2::aes(fill = PPV), colour = "white")
   
   
   # NPV ---------------------------------------------------------------------
   
-          
       } else if (PPV_NPV == "NPV") {
+
+        # Create plot
+        p = ggplot2::ggplot(PPV_melted, ggplot2::aes(FN, (Prevalence)))  
 
         # USE PPV_melted to get this!!!!
         breaks_x = round(seq(.GlobalEnv$Min_FN, Max_FN, Step_size_FN * 10), decimals_x)
@@ -76,12 +85,12 @@
         labels_y = paste(Min_Prevalence, .GlobalEnv$prevalence_label, breaks_y)
         # labels_y = paste(Min_Prevalence, prevalence_label, round(unique(PPV_melted$Prevalence)[c(seq(1, 100, 10), 101)], 0))
         
-        # Create plot
-        p = ggplot2::ggplot(PPV_melted, ggplot2::aes(FN, Prevalence)) + 
-          ggplot2::geom_tile(ggplot2::aes(fill = NPV), colour = "white")
+        # NPV tiles
+        p = p + ggplot2::geom_tile(ggplot2::aes(fill = NPV), colour = "white")
         
       }
       
+    # add 
     p = p + 
       ggplot2::scale_x_continuous(breaks = breaks_x, labels = labels_x, expand = c(0,0)) + 
       ggplot2::scale_y_continuous(breaks = breaks_y, labels = labels_y, expand = c(0,0)) +
